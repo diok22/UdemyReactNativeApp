5ca5970615ca0d4a7c094644aa2294c61606d5e5
diok22 <Dionysis.k22@gmail.com>
2016-12-26 15:23:58 +0200

    redering some text in ios

---
+++ albums/index.ios.js
@@ -0,0 +1,13 @@
+// Place code here for developing for IOS!!
+
+// Import a library to help create a component
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
+
+// Create a component
+const App = () => (
+    <Text>Some text</Text>
+  );
+
+// Render it to the device
+AppRegistry.registerComponent('albums', () => App);
